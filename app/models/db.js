654db5f453c4a970a9b468a11f8bd531282c06be
Nguyễn Quang Thắng <118097630+ThangKA-38@users.noreply.github.com>
2023-11-12 21:41:51 +0700

    change config

--- app/models/db.js
+++ app/models/db.js
@@ -2,15 +2,10 @@ const mysql = require('mysql');
 const dbconfig = require('../config/db.config');
 
 const db = mysql.createConnection({
-    // host: dbconfig.HOST,
-    // user: dbconfig.USER,
-    // password: dbconfig.PASSWORD,
-    // database: dbconfig.DB
-
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'vt_book',
+    host: dbconfig.HOST,
+    user: dbconfig.USER,
+    password: dbconfig.PASSWORD,
+    database: dbconfig.DATABASE
 });
 
 db.connect(error => {
